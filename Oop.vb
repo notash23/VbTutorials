d0eac25370083673e77781629dc46c18d08b676b
notash23 <akashdwarka51@gmail.com>
2024-06-24 14:02:52 +0400

    Added Oop

---
+++ Oop.vb
@@ -0,0 +1,115 @@
+﻿Module Oop
+
+    ' A class is a way to encapsulate related data and codes together
+    Class Deck
+        ' Attribute
+        Public cards(52) As Card
+
+        ' Methods
+        Public Overloads Sub PrintCardNumbers()
+            For i = 1 To 52
+
+                ' We should access the card number using the getter function
+                Console.WriteLine("The Card number is " & cards(i).getCardNumber & " and the card type is " & cards(i).cardType)
+            Next
+        End Sub
+
+        Public Overloads Sub PrintCardNumbers(filterCardType As String)
+            For i = 1 To 52
+                ' Overloading the previous method to now use a filter which is taken as parameter
+                If cards(i).cardType = filterCardType Then
+                    Console.WriteLine("The Card number is " & cards(i).getCardNumber & " and the card type is " & cards(i).cardType)
+                End If
+            Next
+        End Sub
+
+        Public Overloads Sub PrintCardNumbers(range As Integer)
+            For i = 1 To range
+                ' Overloading the method again to take an integer as parameter and print that range
+                Console.WriteLine("The Card number is " & cards(i).getCardNumber & " and the card type is " & cards(i).cardType)
+            Next
+        End Sub
+
+        Public Sub New()
+
+            Dim types = {"Spade", "Heart", "Clover", "Diamond"}
+            ' Constructors
+
+            For j = 0 To 3
+                For i = j * 13 + 1 To j * 13 + 13
+                    If i = j * 13 + 1 Then
+                        cards(i) = New Card("A", types(j))
+                    ElseIf i = j * 13 + 11 Then
+                        cards(i) = New Card("J", types(j))
+                    ElseIf i = j * 13 + 12 Then
+                        cards(i) = New Card("Q", types(j))
+                    ElseIf i = j * 13 + 13 Then
+                        cards(i) = New Card("K", types(j))
+                    Else
+                        cards(i) = New Card((i - j * 13).ToString, types(j))
+                    End If
+                Next
+            Next
+        End Sub
+    End Class
+
+    Class Card
+        ' Attributes
+        Public cardType As String
+
+        ' Properties (can be acccessed outside the class only using the getter and setter)
+        Private cardNumber As String
+
+        ' Methods
+
+        'Getter
+        Public Function getCardNumber() As String
+            Return cardNumber
+        End Function
+
+        ' Setter
+        Public Sub setCardNumber(newCardNumber As String)
+            cardNumber = newCardNumber
+        End Sub
+
+        ' Overridable function (Polymorphism, i.e this method can be changed when the class is inherited)
+        Public Overridable Function IsCardHigher(otherCard As Card) As Boolean
+            ' This function returns true if our card's number is higher than the other card's number
+            If (cardNumber > otherCard.cardNumber) Then
+                Return True
+            Else
+                Return False
+            End If
+        End Function
+
+        Public Sub New(p_cardNumber As String, p_cardType As String)
+            ' Constructor
+            cardNumber = p_cardNumber
+            cardType = p_cardType
+        End Sub
+    End Class
+
+    ' Inheritance
+    Class UnoCard : Inherits Card
+        Public isWildCard As Boolean
+
+        'methods
+        Public Sub New(p_cardNumber As String, p_cardType As String, p_isWildCard As Boolean)
+            MyBase.New(p_cardNumber, p_cardType)
+            isWildCard = p_isWildCard
+        End Sub
+
+        'Overriding the isCardHigher function to modify for Uno's needs
+        Public Overrides Function IsCardHigher(otherCard As Card) As Boolean
+            ' Add another condition: I check if this card is a wild card before comparing the card number
+            If isWildCard = True Then
+                Return True
+            Else
+                ' if it is not a wild card, do the same code as written above
+                Return MyBase.IsCardHigher(otherCard)
+            End If
+        End Function
+    End Class
+
+    ' Overloading methods
+End Module
--- Program.vb
+++ Program.vb
@@ -1,64 +1,19 @@
 Module Program
 
-    Dim numbers(15) As Integer
-
-
-    Sub initArray()
-        numbers(0) = 1
-        numbers(1) = 2
-        numbers(2) = 21
-        numbers(3) = 23
-        numbers(4) = 51
-        numbers(5) = 63
-        numbers(6) = 72
-        numbers(7) = 80
-        numbers(8) = 81
-        numbers(9) = 82
-        numbers(10) = 101
-        numbers(11) = 103
-        numbers(12) = 121
-        numbers(13) = 163
-        numbers(14) = 172
-        numbers(15) = 180
-    End Sub
-
-
-
     Sub Main(args As String())
-        initArray()
-        ' Let's say we want to find the number 3 in the array
-        ' There are two ways to do it
-
-
-        ' This linear search is O(n)
-        Dim isFound As Boolean = False
-        Dim index As Integer
-
-        While (isFound = False And index < numbers.Length)
-            If (numbers(index) = 82) Then
-                Console.WriteLine("Found value at index " & index)
-                isFound = True
-            End If
-            index = index + 1
-        End While
-
-        ' The binary search is O(lg(n))
-        ' So, it is comparatively faster for large array sizes
-        Dim minIndex As Integer = 0
-        Dim maxIndex As Integer = numbers.Length - 1
-        isFound = False
-
-        Dim middleIndex As Integer = (minIndex + maxIndex) \ 2
-        While (minIndex < maxIndex - 1 And isFound = False)
-            middleIndex = (minIndex + maxIndex) \ 2
-            If (numbers(middleIndex) > 82) Then
-                maxIndex = middleIndex
-            ElseIf (numbers(middleIndex) < 82) Then
-                minIndex = middleIndex
-            Else
-                Console.Write("Found value at index " & middleIndex)
-                isFound = True
-            End If
-        End While
+        Dim newDeck As Deck
+        newDeck = New Deck()
+        newDeck.PrintCardNumbers()
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        newDeck.PrintCardNumbers("Diamond")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        newDeck.PrintCardNumbers(11)
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
     End Sub
 End Module
